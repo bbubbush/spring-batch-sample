6f68c3f6ea69b92f3dab996e4161689a13a76517
bbubbush <bbubbush@gmail.com>
2024-03-02 20:29:00 +0900

    init

---
+++ src/main/java/com/bbubbush/batch/SpringBatchSampleApplication.java
@@ -0,0 +1,13 @@
+package com.bbubbush.batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBatchSampleApplication.class, args);
+  }
+
+}
